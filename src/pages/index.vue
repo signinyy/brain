270fda741ed02a3cb20148fcf28418bfd6e4726b
signinyy <1740315553@qq.com>
2024-02-25 19:36:02 +0800

    second commit

---
+++ src/pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Welcome to My Nuxt 3 App</h1>
+  </div>
+</template>
+
+<script setup>
+// 这里可以放置你的组件逻辑
+</script>
+
+<style scoped>
+/* 这里可以放置你的组件样式 */
+h1 {
+  color: blue;
+}
+</style>
